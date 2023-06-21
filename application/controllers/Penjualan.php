54246c23d54153731a61da056a1dc84cb1cee357
aksan-kurdin <aksan.kurdin@gmail.com>
2023-06-21 16:54:51 +0700

    Export to Excel

--- application/controllers/Penjualan.php
+++ application/controllers/Penjualan.php
@@ -160,6 +160,13 @@ class Penjualan extends CI_Controller
         $data['tgl_mulai'] = $tgl_mulai;
         $data['tgl_akhir'] = $tgl_akhir;
         $data['rpt_data'] = $this->M_penjualan->get_reportdata($cabang, $tgl_mulai, $tgl_akhir)->result();
+        if (isset($_POST['export'])) {
+            // Fungsi header dengan mengirimkan raw data excel
+            header("Content-type: application/vnd-ms-excel");
+
+            // Mendefinisikan nama file ekspor "hasil-export.xls"
+            header("Content-Disposition: attachment; filename=Laporan Penjualan.xls");
+        }
         $this->load->view('penjualan/r_penjualan', $data);
     }
 
